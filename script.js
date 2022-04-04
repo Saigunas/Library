cce6013d5017357b300b209fa3750a0cd58db312
Saigunas <saigunasb777@gmail.com>
2022-04-04 18:07:47 +0300

    Has default books, displays info

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const library =  document.querySelector('.library');
 
 let myLibrary = [];
 
+setDefaultBooks();
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -9,34 +11,93 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
-function addBookToLibrary() {
-  // do stuff here
-}
 
 function readInput() {
-    while(true) {
-        let stop = prompt('stop?(y/n)');
-        if(stop === 'y') break;
-        let title = prompt('title');
-        let author = prompt('author');
-        let pages = prompt('pages');
-        let read = prompt('read');
+  while(true) {
+    let stop = prompt('stop?(y/n)');
+    if(stop === 'y') break;
+    let title = prompt('title');
+    let author = prompt('author');
+    let pages = prompt('pages');
+    let read = prompt('read');
+    
+    let newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+  }
+  showLibrary();
+}
 
-        let newBook = new Book(title, author, pages, read);
-        myLibrary.push(newBook);
-    }
-    showLibrary();
+function setDefaultBooks() {
+  let newBook = new Book("The Last Wish", "Andrzej Sapkowski", '359', 'Read');
+  myLibrary.push(newBook);
+
+  newBook = new Book("World War Z", "Max Brooks", '342', 'Reading');
+  myLibrary.push(newBook);
+
+  newBook = new Book("The Shining", "Stephen King", '659', 'New');
+  myLibrary.push(newBook);
+
+  showLibrary();
 }
 
 function showLibrary() {
-    console.table(myLibrary);
-    for(let index = 0; index < myLibrary.length; index++) {        
-        let book = document.createElement('div');
-        book.classList.add('book');
-        book.setAttribute(`data-book${index}`, "true");
-        book.textContent = 'Book added';
-        library.appendChild(book);
-    }
+  console.table(myLibrary);
+  for(let index = 0; index < myLibrary.length; index++) {        
+    addBookToLibrary(index);
+  }
 }
 
-readInput();
+function addBookToLibrary(index) {
+  let book = document.createElement('div');
+  book.classList.add('book');
+  book.setAttribute(`data-book${index}`, "true");
+    
+  let title = document.createElement('h2');
+  title.classList.add('title');
+  title.textContent = myLibrary[index].title;
+
+  let author = document.createElement('h3');
+  author.classList.add('author');
+  author.textContent = myLibrary[index].author;
+
+  let pages = document.createElement('p');
+  pages.classList.add('pages');
+  pages.textContent = myLibrary[index].pages;
+
+  let status = document.createElement('select');
+      
+  let optionNew = document.createElement('option');
+  optionNew.textContent = "New";
+
+  let optionReading = document.createElement('option');
+  optionReading.textContent = "Reading";
+
+  let optionRead = document.createElement('option');
+  optionRead.textContent = "Read";
+      
+  switch(myLibrary[index].read) {
+    case "New":
+      optionNew.selected = true;
+      break;
+    case "Reading":
+      optionReading.selected = true;
+      break;
+    default:
+      optionRead.selected = true;
+  }
+
+  status.appendChild(optionNew);
+  status.appendChild(optionReading);
+  status.appendChild(optionRead);
+
+  let remove = document.createElement('img');
+  remove.src = "/icons/close-box.svg";
+
+  book.appendChild(title);
+  book.appendChild(author);
+  book.appendChild(pages);
+  book.appendChild(status);
+  book.appendChild(remove);
+
+ library.appendChild(book);
+}
